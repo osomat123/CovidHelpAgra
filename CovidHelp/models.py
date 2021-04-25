25cc3bbb37deac35936aabfa7d92175f1de41b1f
Tejpunj Raju <tejpunjraju8a@gmail.com>
2021-04-26 00:56:53 +0530

    Resource form Backend complete

--- CovidHelp/models.py
+++ CovidHelp/models.py
@@ -3,10 +3,9 @@ import datetime
 
 class Resource(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    phone = db.Column(db.String(50))
-    email = db.Column(db.String(50))
-    details = db.Column(db.String(100))
+    name = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.String(50), nullable=False)
+    details = db.Column(db.String(100), nullable=False)
     link = db.Column(db.String(100))
 
     def __repr__(self):
@@ -14,7 +13,7 @@ class Resource(db.Model):
 
 class Service(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
         return f"Service({self.id}, {self.name})"
--- CovidHelp/routes.py
+++ CovidHelp/routes.py
@@ -1,16 +1,34 @@
-from flask import render_template, url_for,redirect, request
+from flask import render_template, url_for,redirect, request, flash
 from CovidHelp import app, db
 from CovidHelp.forms import *
 from CovidHelp.models import *
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/ResourceForm', methods=['GET', 'POST'])
 def resource_form():
     form = ResourceForm()
 
     if form.validate_on_submit():
-        return 'Yayyyyy!!!'
+
+        resource = Resource(name=form.name.data,
+                            phone=form.phone.data,
+                            details=form.details.data,
+                            link=form.link.data)
+        db.session.add(resource)
+        db.session.commit()
+        
+        for service_type in form.type.data:
+            service = Service.query.filter_by(name=service_type).first()
+            avail = Availability(resource_id=resource.id, service_id=service.id)
+            db.session.add(avail)
+            db.session.commit()
+
+        flash("Resource Added Successfully", 'flash_success')
+        return redirect(url_for('home'))
+
+    else:
+        flash("Please fill the form correctly", 'flash_fail')
     return render_template('form.html', form=form)
