ec590f62c3937daa1a2dfad803ef65b914862947
Tejpunj Raju <tejpunjraju8a@gmail.com>
2021-04-30 20:23:01 +0530

    Some changes

--- CovidHelp/__init__.py
+++ CovidHelp/__init__.py
@@ -7,7 +7,7 @@ app.config['SECRET_KEY'] = '1afd28ad58fb19cb088674114a5e9af9'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 # For PostgreSQL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://<username>:<password>@localhost/<DB Name>'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:steelchair111@localhost/CovidHelpAgra'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
